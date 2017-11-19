7f5a0eeb3cc2a2f28db5f7be4d9a0cd1eed1b58e
Nadir Benallal <nadir.benallal@heig-vd.ch>
2017-11-19 22:25:09 +0100

    Adding radioButtons to select the file containing the JSON to compress in CompressedSendRequestActivity.java

--- app/src/main/java/ch/heig/sym_labo2/activities/CompressedSendRequestActivity.java
+++ app/src/main/java/ch/heig/sym_labo2/activities/CompressedSendRequestActivity.java
@@ -1,10 +1,13 @@
 package ch.heig.sym_labo2.activities;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import ch.heig.sym_labo2.R;
@@ -18,7 +21,8 @@ public class CompressedSendRequestActivity extends SCMActivities {
 
     // Attributs
     private Button sendButton = null;
-    private EditText inputText = null;
+    private RadioGroup fileChoice = null;
+    private RadioButton fileChose = null;
     private TextView responseText = null;
     private SymComManager symComManager = null;
 
@@ -28,7 +32,7 @@ public class CompressedSendRequestActivity extends SCMActivities {
         setContentView(R.layout.activity_compressed_send_request);
 
         sendButton = (Button) findViewById(R.id.compressedSendButton);
-        inputText = (EditText) findViewById(R.id.compressedInputText);
+        fileChoice = (RadioGroup) findViewById(R.id.compressedFilesRadioGroup);
         responseText = (TextView) findViewById(R.id.compressedResponseFromServerTextView);
         responseText.setMovementMethod(new ScrollingMovementMethod());
 
@@ -37,9 +41,12 @@ public class CompressedSendRequestActivity extends SCMActivities {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int checkedRadioButtonID = fileChoice.getCheckedRadioButtonId();
+                fileChose = (RadioButton) findViewById(checkedRadioButtonID);
+
                 try {
-                    //Envoi de la requête contenant le texte saisi par l'utilisateur au serveur
-                    symComManager.sendRequest(inputText.getText().toString(), "http://sym.iict.ch/rest/json");
+                    //Envoi de la requête contenant le JSON contenue dans le fichier choisi par l'utilisateur au serveur
+                    symComManager.sendRequest(fileChose.getText().toString(), "http://sym.iict.ch/rest/json");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
